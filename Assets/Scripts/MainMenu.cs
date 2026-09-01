c26748c9344ee2080db34f41279ff373ef72dd92
Baruska <164750314+baruskaa@users.noreply.github.com>
2026-09-01 21:04:06 +0800

    main menu pero may bug huhuhu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public Animator transition;
+    public float transitionTime = 1f;
+    public void Play()
+    {
+        LoadNextLevel();
+    }
+    public void Quit()
+    {
+
+        Application.Quit();
+        Debug.Log("app quit");
+    }
+    public void LoadNextLevel()
+    {
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+    public void LoadLevelByIndex(int buildIndex)
+    {
+        StartCoroutine(LoadLevel(buildIndex));
+    }
+    private IEnumerator LoadLevel(int levelIndex)
+    {
+        transition.SetTrigger("Start");
+        yield return new WaitForSeconds(transitionTime);
+        SceneManager.LoadScene(levelIndex);
+    }
+}
