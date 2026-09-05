acabcf98f86072051857fdcf85abd4b2be4e3d9c
Baruska <164750314+baruskaa@users.noreply.github.com>
2026-09-06 00:01:49 +0800

    inventory select pa lang maygad

--- Assets/Scripts/UI/InventoryItem.cs
+++ Assets/Scripts/UI/InventoryItem.cs
@@ -1,16 +1,79 @@
+using System;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using TMPro;
 
 public class InventoryItem : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField]
+    private Image itemImage;
+
+
+    [SerializeField]
+    private Image borderImage;
+
+    public event Action<InventoryItem> OnItemClicked,
+        OnItemDroppedOn, OnItemBeginDrag, OnItemEndDrag, OnRightMouseBtnClick;
+
+    private bool empty = true;
+
+    public void Awake()
     {
-        
+        ResetData();
+        Deselect();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ResetData()
     {
-        
+        this.itemImage.gameObject.SetActive(false);
+        empty = true;
+    }
+
+    public void Deselect()
+    {
+        borderImage.enabled = false;
+    }
+
+    public void SetData(Sprite sprite)
+    {
+        this.itemImage.gameObject.SetActive(true);
+        this.itemImage.sprite = sprite;
+        empty = false;
+    }
+
+    public void Select()
+    {
+        borderImage.enabled = true;
+    }
+
+    public void OnBeginDrag(BaseEventData eventData)
+    {
+        if (empty)
+            return;
+        OnItemBeginDrag?.Invoke(this);
+    }
+
+    public void OnDrop(BaseEventData eventData)
+    {
+        OnItemDroppedOn?.Invoke(this);
+    }
+
+    public void OnEndDrag(BaseEventData eventData)
+    {
+        OnItemEndDrag?.Invoke(this);
+    }
+
+    public void OnPointerClick(BaseEventData data)
+    {
+        PointerEventData pointerData = (PointerEventData)data;
+        if (pointerData.button == PointerEventData.InputButton.Right)
+        {
+            OnRightMouseBtnClick?.Invoke(this);
+        }
+        else
+        {
+            OnItemClicked?.Invoke(this);
+        }
     }
 }
--- Assets/Scripts/UI/InventoryPage.cs
+++ Assets/Scripts/UI/InventoryPage.cs
@@ -1,5 +1,8 @@
+using Inventory.UI;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InventoryPage : MonoBehaviour
 {
@@ -10,11 +13,27 @@ public class InventoryPage : MonoBehaviour
     private RectTransform contentPanel;
 
     [SerializeField]
+    private InventoryDescription itemDescription;
+
+
+    [SerializeField]
     private Animator animator;
 
     List<InventoryItem> listOfUIItems = new List<InventoryItem>();
 
+    public Sprite image;
+    public string title, description;
+
     private bool isOpen = false;
+
+
+    private void Awake()
+    {
+        Idle();
+        itemDescription.ResetDescription();
+    }
+
+
 
     public void InitializeInventoryUI(int inventorysize)
     {
@@ -24,12 +43,42 @@ public class InventoryPage : MonoBehaviour
                 Instantiate(itemPrefab, Vector3.zero, Quaternion.identity);
             uiItem.transform.SetParent(contentPanel);
             listOfUIItems.Add(uiItem);
+            uiItem.OnItemClicked += HandleItemSelection;
+            uiItem.OnItemBeginDrag += HandleBeginDrag;
+            uiItem.OnItemDroppedOn += HandleSwap;
+            uiItem.OnItemEndDrag += HandleEndDrag;
+            uiItem.OnRightMouseBtnClick += HandleShowItemActions;
         }
+    }
+
+    private void HandleShowItemActions(InventoryItem item)
+    {
+    }
+
+    private void HandleEndDrag(InventoryItem item)
+    {
+    }
+
+    private void HandleSwap(InventoryItem item)
+    {
+    }
+
+    private void HandleBeginDrag(InventoryItem item)
+    {
+    }
+
+    private void HandleItemSelection(InventoryItem item)
+    {
+        itemDescription.SetDescription(image, title, description);
+        listOfUIItems[0].Select();
     }
 
     public void Show()
     {
         isOpen = true;
+        itemDescription.ResetDescription();
+
+        listOfUIItems[0].SetData(image);
         animator.Play("Show");
     }
 
@@ -37,6 +86,11 @@ public class InventoryPage : MonoBehaviour
     {
         isOpen = false;
         animator.Play("Hide"); 
+    }
+
+    public void Idle()
+    {
+        animator.Play("Idle");
     }
 
     public bool IsOpen()
